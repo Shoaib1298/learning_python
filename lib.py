a6f223d0044d9da67f43faceac01a83635e50082
Shoaib1298 <154250778+Shoaib1298@users.noreply.github.com>
2024-05-01 22:27:28 +0530

    Update lib.py

--- lib.py
+++ lib.py
@@ -2,59 +2,50 @@ import random
 import sys
 import cowsay
 
-# todo random.choice
-# choin = random.choice(["up", "down"])
+todo random.choice
+choin = random.choice(["up", "down"])
 
-# print(choin)
+print(choin)
 
-# num = random.randint(1, 10)
+num = random.randint(1, 10)
 
-# print(num)
+print(num)
 
-# todo random.shuffle
+todo random.shuffle
 
-# fruits = ["apple","banana","orange"]
+fruits = ["apple","banana","orange"]
 
-# random.shuffle(fruits)
+random.shuffle(fruits)
 
-# print(fruits)
+print(fruits)
 
-# print("my name is :",sys.argv[1], ", and this is written in file :",sys.argv[0])
+print("my name is :",sys.argv[1], ", and this is written in file :",sys.argv[0])
 
 
 #! learning try
 
-# try :
-#     print("my name is :",sys.argv[1], ", and this is written in file :",sys.argv[0])
-# except IndexError:
-#     print("please write ur name")
+try :
+    print("my name is :",sys.argv[1], ", and this is written in file :",sys.argv[0])
+except IndexError:
+    print("please write ur name")
 
-# if len(sys.argv) < 2:
-#     sys.exit("too few arg")
-# if len(sys.argv) > 2:
-#     sys.exit("too many arg")
+if len(sys.argv) < 2:
+    sys.exit("too few arg")
+if len(sys.argv) > 2:
+    sys.exit("too many arg")
 
-# print("my name is :",sys.argv[1], ", and this is written in file :",sys.argv[0])
+print("my name is :",sys.argv[1], ", and this is written in file :",sys.argv[0])
 
-# ? what is difference b/w list and array and vector
 
 #! learning scaling an list
 
-# if len(sys.argv) < 2:
-#     sys.exit("too few arg")
+if len(sys.argv) < 2:
+    sys.exit("too few arg")
 
-# for arg in sys.argv[1:] :
+for arg in sys.argv[1:] :
 
-#     print("name of coder is :",arg)
+    print("name of coder is :",arg)
 
-
-# Better_comments
-
-# ? why is this true
-# ! wait a ,something is wrong
-# //my name is khan
-# *apple
-# todo banana
 
 #! arr[-1] mean last element but the notation differs in slicing
 
